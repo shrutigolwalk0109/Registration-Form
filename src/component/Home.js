c609e2e1d6f5230f73c361c09e4cdd9182fde810
Shruti Golwalkar <132553685+shrutigolwalk0109@users.noreply.github.com>
2023-11-01 20:48:36 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,3 @@
-import cloud from "../website/main-photo/cloud.png";
-import star from "../website/main-photo/star.png"
-import wstar from "../website/main-photo/w-star.png"
-
-
 export function Home() {
   return (
     <>
@@ -11,40 +6,40 @@ export function Home() {
           <div className="row ">
             {/* heading */}
             <div className="background">
-              <h3 className=" heading text-center top mb-3 ">
-                <u>REGISTRATION FORM</u>
+              <h3 className=" heading text-center top  ">
+                <u>Registration Form</u>
               </h3>
               <div className="fixed cloud-img  ">
-                <img src={cloud} alt="cloud-img" />
+                {/* <img src={cloud} alt="cloud-img" /> */}
               </div>
             </div>
             <br></br>
 
             {/* Three-Stars col-md-2*/}
             <div className="col-md-2 stars star">
-            <img className="fixed" src={star} alt="star-img" />
+            {/* <img className="fixed" src={star} alt="star-img" /> */}
             </div>
 
             {/* Main-Form col-md-8 */}
             <form className="form col-md-8" >
 
-              <p>1. Full Name</p>
-              <input className="name" type="text" name="n" placeholder="   First name" required ></input>
-              <input className="name" type="text" name="n" placeholder="   Last name" required ></input>
+              <p><span>1.</span> Full Name</p>
+              <input className="input" type="text" name="n" placeholder="   Enter your name" required ></input>
+              
               <br />
               <br />
 
-              <p>2. Email Address</p>
+              <p><span>2.</span> Email Address</p>
               <input className="input" type="email" name="n" placeholder="   Enter your email" required ></input>
               <br />
               <br />
 
-              <p>3. Phone Number</p>
+              <p><span>3.</span> Phone Number</p>
               <input className="input" type="number" name="n" placeholder="   Enter your number" required></input>
               <br />
               <br />
 
-              <p>4. Select your Branch</p>
+              <p><span>4.</span> Select your Branch</p>
               <select class="form-select select" placeholder="   Select" aria-label="Default select example">
                 <option selected></option>
                 <option value="1">Computer Science Engineering</option>
@@ -57,7 +52,7 @@ export function Home() {
               <br />
 
 
-              <p>5. Select your Study Year</p>
+              <p><span>5.</span> Select your Study Year</p>
               <select class="form-select select" placeholder="   Select" aria-label="Default select example">
                 <option selected></option>
                 <option value="1">1st year</option>
@@ -70,7 +65,7 @@ export function Home() {
               <br />
               <br />
 
-              <p>6. Are you currently a member of any other college club?</p>
+              <p><span>6.</span> Are you currently a member of any other college club?</p>
               <select class="form-select select" placeholder="   Select" aria-label="Default select example">
                 <option selected></option>
                 <option value="1">Yes</option>
@@ -80,12 +75,12 @@ export function Home() {
               <br />
               <br />
 
-              <p>7. Please briefly describe why you are interested in joining this college club.</p>
+              <p><span>7.</span> Please briefly describe why you are interested in joining this college club.</p>
               <textarea className="input" cols={67} style={{ height: 100 }}></textarea>
               <br />
               <br />
                
-              <p>8. Do you have any previous experience or skills related to the activities of this college club?</p>
+              <p><span>8.</span> Do you have any previous experience or skills related to the activities of this college club?</p>
               <select class="form-select select" placeholder="   Select" aria-label="Default select example">
                 <option selected></option>
                 <option value="1">Yes</option>
@@ -94,13 +89,13 @@ export function Home() {
               <br />
               <br />
 
-              <p>9. If yes, please describe your previous experience or skills.</p>
+              <p><span>9.</span> If yes, please describe your previous experience or skills.</p>
               <textarea className="input" cols={67} style={{ height: 100 }}></textarea>
               <br />
               <br />
 
 
-              <p>10. Do you have any specific ideas or suggestions for club events or initiatives?</p>
+              <p><span>10.</span> Do you have any specific ideas or suggestions for club events or initiatives?</p>
               <select class="form-select select" placeholder="   Select" aria-label="Default select example">
                 <option selected></option>
                 <option value="1">Yes</option>
@@ -109,27 +104,24 @@ export function Home() {
               <br />
               <br />
 
-              <p>11. Please share your ideas or suggestions..</p>
+              <p><span>11.</span> Please share your ideas or suggestions..</p>
               <textarea className="input" cols={67} style={{ height: 100 }}></textarea>
               <br />
               <br />
 
-              <label for="file">12. If applicable, please attach your resume.</label>
+              <label for="file"><p><span>12.</span> If applicable, please attach your resume.</p></label>
               <input id="file" type="file" className="file"/>
               <br />
               <br />
 
                
-              <button className="Button d-grid gap-2 d-md-block" type="submit" placeholder="Send" form="form1" value="Submit">
+              <button className="Button mb-3"  type="submit" placeholder="Send" form="form1" value="Submit">
                 Submit
               </button>
             </form>
-            <div className="col-md-2 cloud wstar">
-            <img className="fixed" src={wstar} alt="wstar-img" />
+            <div className="col-md-2 cloud">
+            {/* <img className="fixed" src={wstar} alt="wstar-img" /> */}
             </div>
-            
-           
-
        </div>
         </div>
       </body>
